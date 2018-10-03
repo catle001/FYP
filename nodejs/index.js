e4de761a7233990b5477a306400e1bb75ec5619f
thiencat1997 <thiencat1997@gmail.com>
2018-10-03 13:13:28 +0800

    learn nodejs

---
+++ nodejs/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const htmlfile = 'histogram3.html';
+
+const server = http.createServer(function (req, res) {
+	fs.readFile(htmlfile , function(err, data) {
+	    res.writeHead(200, {'Content-Type': 'text/html'});
+	    res.write(data);
+	    res.end('Hello World!');
+	});
+});
+
+server.listen(port, hostname, () =>{
+	console.log('server started on port ' + port);
+});
